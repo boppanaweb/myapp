0904fad4f2d902b24fc78d6d0f6eeb405170b3ed
boppanaweb <45306198+boppanaweb@users.noreply.github.com>
2018-11-24 16:39:52 +0530

    glimer exp

---
+++ glimer example/sample/my-app/src/ui/components/MyGlimmer/component.ts
@@ -0,0 +1,10 @@
+
+import Component,{tracked} from '@glimmer/component';
+
+export default class MyGlimmer extends Component {
+  @tracked private count = 0;
+
+  private increment() {
+      this.count++;
+  }
+}
